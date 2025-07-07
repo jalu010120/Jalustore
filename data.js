194274a4f80b9d99d91bc404f2cf107d5e21c837
jalustore <hamrauchiha@gmail.com>
2025-07-07 19:25:06 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+// js/data.js
+
+const deliveryData = [
+    // Orderan Berjalan
+    { slot: 1, name: 'bulilisso', order: 80, send: 60 },
+    { slot: 1, name: 'Ney', order: 4340, send: 3050 },
+    { slot: 1, name: 'Purili', order: 320, send: 180 },
+    { slot: 1, name: 'Rain', order: 320, send: 180 },
+    { slot: 1, name: 'Nayla', order: 530, send: 358 },
+    { slot: 1, name: 'Aelia', order: 840, send: 280 },
+    { slot: 1, name: 'Raine', order: 420, send: 280 },
+
+    { slot: 2, name: 'Isa', order: 800, send: 40 },
+    { slot: 2, name: 'Angel2', order: 600, send: 400 },
+    { slot: 2, name: 'Alip', order: 320, send: 160 },
+    { slot: 2, name: 'Rani', order: 320, send: 180 },
+    { slot: 2, name: 'Anggi', order: 320, send: 160 }, 
+    { slot: 2, name: 'Autumn', order: 850, send: 96 }, 
+    { slot: 2, name: 'Amanda', order: 80, send: 40 }, 
+
+    // Riwayat Orderan Selesai
+    { slot: 'completed', name: 'Harbie', order: 200, send: 200 },
+    { slot: 'completed', name: 'Fida', order: 100, send: 103 }, 
+    { slot: 'completed', name: 'Vyan', order: 80, send: 83 }, 
+    { slot: 'completed', name: 'Ayu', order: 1000, send: 1003 }, 
+    { slot: 'completed', name: 'Anna', order: 600, send: 603 }, 
+    { slot: 'completed', name: 'Zee', order: 150, send: 153 }, 
+    { slot: 'completed', name: 'Nanda', order: 210, send: 213 }, 
+    { slot: 'completed', name: 'Alvin', order: 210, send: 213 }, 
+    { slot: 'completed', name: 'Ira', order: 150, send: 153 }, 
+    { slot: 'completed', name: 'Kael', order: 120, send: 123 }, 
+    { slot: 'completed', name: 'Xen', order: 210, send: 213 }, 
+    { slot: 'completed', name: 'Lusi', order: 600, send: 603 },
+    { slot: 'completed', name: 'Sea', order: 150, send: 157 } 
+];
