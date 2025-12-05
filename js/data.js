c21d0a7d3a3af6d531fe95e3992da1523f1d2177
jalustore <hamrauchiha@gmail.com>
2025-12-05 22:37:45 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,6 @@
-capek gw ajg
+[
+    { slot: 1, name: 'SHAINA', order: 500, send: 60 },
+    { slot: 1, name: 'ZAH', order: 600, send: 210 },
+    { slot: 2, name: 'RAI', order: 100, send: 32 },
+    { slot: 2, name: 'ACELL', order: 100, send: 40 }
+]
