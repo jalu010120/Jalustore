c8c3f0b07a431b8a375624e581a5b5161df32d6a
jalustore <hamrauchiha@gmail.com>
2025-12-06 21:20:34 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,9 @@
 [
-    { slot: 1, name: 'SHAINA', order: 500, send: 60 },
-    { slot: 1, name: 'ZAH', order: 600, send: 210 },
-    { slot: 2, name: 'RAI', order: 100, send: 32 },
-    { slot: 2, name: 'ACELL', order: 100, send: 40 }
+    { slot: 1, name: 'SHAINA', order: 500, send: 90 },
+    { slot: 1, name: 'ZAH', order: 600, send: 217 },
+    { slot: 1, name: 'JASMINE', order: 500, send: 90 },
+    { slot: 2, name: 'RAI', order: 100, send: 51 },
+    { slot: 2, name: 'ACELL', order: 100, send: 60 },
+    { slot: 2, name: 'IKA', order: 200, send: 20 },
+    { slot: 2, name: 'KIKI', order: 100, send: 20 }
 ]
