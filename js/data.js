077291ae157ae2b592f7cbc7daa633da351de2d8
jalustore <hamrauchiha@gmail.com>
2025-12-07 13:15:59 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,6 +2,8 @@ const deliveryData = [
     { slot: 1, name: 'SHAINA', order: 500, send: 90 },
     { slot: 1, name: 'ZAH', order: 600, send: 217 },
     { slot: 1, name: 'JASMINE', order: 500, send: 90 },
+    { slot: 1, name: 'RESTU', order: 500, send: 250 },
+    { slot: 1, name: 'YAYA', order: 500, send: 70 },
     { slot: 2, name: 'RAI', order: 100, send: 51 },
     { slot: 2, name: 'ACELL', order: 100, send: 60 },
     { slot: 2, name: 'IKA', order: 200, send: 20 },
