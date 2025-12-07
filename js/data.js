2f2694413883cfec810c315ca6728353fbbc5100
jalustore <hamrauchiha@gmail.com>
2025-12-07 17:05:15 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,12 +1,12 @@
 const deliveryData = [
-    { slot: 1, name: 'SHAINA', order: 500, send: 90 },
-    { slot: 1, name: 'ZAH', order: 600, send: 217 },
-    { slot: 1, name: 'JASMINE', order: 500, send: 90 },
-    { slot: 1, name: 'RESTU', order: 500, send: 250 },
-    { slot: 1, name: 'YAYA', order: 500, send: 70 },
-    { slot: 2, name: 'RAI', order: 100, send: 51 },
-    { slot: 2, name: 'ACELL', order: 100, send: 60 },
-    { slot: 2, name: 'IKA', order: 200, send: 20 },
-    { slot: 2, name: 'KIKI', order: 100, send: 20 },
-    { slot: 2, name: 'GIN', order: 100, send: 40 }
+    { slot: 1, name: 'SHAINA', order: 500, send: 120 },
+    { slot: 1, name: 'ZAH', order: 600, send: 247 },
+    { slot: 1, name: 'JASMINE', order: 500, send: 120 },
+    { slot: 1, name: 'RESTU', order: 500, send: 280 },
+    { slot: 1, name: 'YAYA', order: 500, send: 100 },
+    { slot: 2, name: 'RAI', order: 100, send: 71 },
+    { slot: 2, name: 'ACELL', order: 100, send: 60, status: '❌' },
+    { slot: 2, name: 'IKA', order: 200, send: 40 },
+    { slot: 2, name: 'KIKI', order: 100, send: 40 },
+    { slot: 2, name: 'GIN', order: 100, send: 60 }
 ];
