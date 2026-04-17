198f3b066457cf40c05cf6c9171fbc8bfee9069c
jalustore <hamrauchiha@gmail.com>
2026-04-18 00:05:18 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+// File: data.js
+// Nanti tiap hari kamu cuma perlu buka dan edit file ini aja ya bang!
+
+const TRACKING_DATA = [
+    // S1
+    { slot: "S1", name: "GANZAI", order: 400, kirim: 419 },
+    { slot: "S1", name: "CUPIS", order: 250, kirim: 210 },
+    { slot: "S1", name: "SHUNA", order: 550, kirim: 380 },
+    { slot: "S1", name: "FEBY", order: 500, kirim: 310 },
+    { slot: "S1", name: "NAI", order: 250, kirim: 220 },
+    { slot: "S1", name: "LIN", order: 250, kirim: 250 },
+    
+    // S2
+    { slot: "S2", name: "CACA", order: 250, kirim: 160 },
+    { slot: "S2", name: "BRIYAN", order: 100, kirim: 100 },
+    { slot: "S2", name: "IKA", order: 200, kirim: 206 },
+    { slot: "S2", name: "DIAN", order: 500, kirim: 200 },
+    { slot: "S2", name: "SHIN", order: 250, kirim: 120 },
+    { slot: "S2", name: "Isma", order: 250, kirim: 190 },
+    { slot: "S2", name: "Sqa", order: 100, kirim: 100 },
+    { slot: "S2", name: "Leya", order: 100, kirim: 100 },
+    { slot: "S2", name: "Mai", order: 250, kirim: 100 },
+    { slot: "S2", name: "Al", order: 250, kirim: 80 },
+    { slot: "S2", name: "Nura", order: 100, kirim: 60 }
+];
