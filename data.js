2386e5b3e449eb470ff6f0508293e165b1eddca5
jalustore <hamrauchiha@gmail.com>
2026-04-19 20:30:33 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 // File: data.js
-// Update Terakhir: Sesuai laporan terbaru
+// Update Terakhir: 19 April 2026
 
 const TRACKING_DATA = [
     // S1
     { slot: "S1", name: "GANZAI", order: 400, kirim: 419 },
     { slot: "S1", name: "CUPIS", order: 250, kirim: 210 },
-    { slot: "S1", name: "SHUNA", order: 550, kirim: 400 }, // Naik dari 380
-    { slot: "S1", name: "FEBY", order: 500, kirim: 330 },  // Naik dari 310
-    { slot: "S1", name: "NAI", order: 250, kirim: 240 },   // Naik dari 220
-    { slot: "S1", name: "LIN", order: 250, kirim: 270 },   // Naik dari 250
+    { slot: "S1", name: "SHUNA", order: 550, kirim: 420 }, // Progres (+20)
+    { slot: "S1", name: "FEBY", order: 500, kirim: 350 },  // Progres (+20)
+    { slot: "S1", name: "NAI", order: 250, kirim: 240 },   
+    { slot: "S1", name: "LIN", order: 250, kirim: 250 },   // SELESAI (✅)
     
     // S2
     { slot: "S2", name: "CACA", order: 250, kirim: 160 },
@@ -16,11 +16,10 @@ const TRACKING_DATA = [
     { slot: "S2", name: "IKA", order: 200, kirim: 206 },
     { slot: "S2", name: "DIAN", order: 500, kirim: 200 },
     { slot: "S2", name: "SHIN", order: 250, kirim: 120 },
-    { slot: "S2", name: "Isma", order: 250, kirim: 210 },  // Naik dari 190
+    { slot: "S2", name: "Isma", order: 250, kirim: 230 },  // Progres (+20)
     { slot: "S2", name: "Sqa", order: 100, kirim: 100 },
     { slot: "S2", name: "Leya", order: 100, kirim: 100 },
-    { slot: "S2", name: "Mai", order: 250, kirim: 120 },   // Naik dari 100
-    { slot: "S2", name: "Al", order: 250, kirim: 100 },    // Naik dari 80
-    { slot: "S2", name: "Nura", order: 100, kirim: 60 }
+    { slot: "S2", name: "Mai", order: 250, kirim: 140 },   // Progres (+20)
+    { slot: "S2", name: "Al", order: 250, kirim: 120 },    // Progres (+20)
+    { slot: "S2", name: "Nura", order: 100, kirim: 80 }    // Progres (+20)
 ];
-
