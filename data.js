72c93bbd1494f9b73c6eeb4012952e90f7ccb8d8
jalustore <hamrauchiha@gmail.com>
2026-04-21 09:05:51 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 // File: data.js
-// Update Terakhir: 19 April 2026
+// Update Terakhir: 21 April 2026
 
 const TRACKING_DATA = [
     // S1
     { slot: "S1", name: "GANZAI", order: 400, kirim: 419 },
     { slot: "S1", name: "CUPIS", order: 250, kirim: 210 },
-    { slot: "S1", name: "SHUNA", order: 550, kirim: 420 }, // Progres (+20)
-    { slot: "S1", name: "FEBY", order: 500, kirim: 350 },  // Progres (+20)
-    { slot: "S1", name: "NAI", order: 250, kirim: 240 },   
-    { slot: "S1", name: "LIN", order: 250, kirim: 250 },   // SELESAI (✅)
+    { slot: "S1", name: "SHUNA", order: 550, kirim: 440 }, // Progres (+20)
+    { slot: "S1", name: "FEBY", order: 500, kirim: 350 },
+    { slot: "S1", name: "NAI", order: 250, kirim: 260 },   // SELESAI (✅)
+    { slot: "S1", name: "LIN", order: 250, kirim: 250 },
     
     // S2
     { slot: "S2", name: "CACA", order: 250, kirim: 160 },
@@ -16,10 +16,17 @@ const TRACKING_DATA = [
     { slot: "S2", name: "IKA", order: 200, kirim: 206 },
     { slot: "S2", name: "DIAN", order: 500, kirim: 200 },
     { slot: "S2", name: "SHIN", order: 250, kirim: 120 },
-    { slot: "S2", name: "Isma", order: 250, kirim: 230 },  // Progres (+20)
+    { slot: "S2", name: "Isma", order: 250, kirim: 250 },  // SELESAI (✅)
     { slot: "S2", name: "Sqa", order: 100, kirim: 100 },
     { slot: "S2", name: "Leya", order: 100, kirim: 100 },
-    { slot: "S2", name: "Mai", order: 250, kirim: 140 },   // Progres (+20)
-    { slot: "S2", name: "Al", order: 250, kirim: 120 },    // Progres (+20)
-    { slot: "S2", name: "Nura", order: 100, kirim: 80 }    // Progres (+20)
+    { slot: "S2", name: "Mai", order: 250, kirim: 160 },   // Progres (+20)
+    { slot: "S2", name: "Al", order: 250, kirim: 140 },    // Progres (+20)
+    { slot: "S2", name: "Nura", order: 100, kirim: 100 },  // SELESAI (✅)
+    { slot: "S2", name: "ellie", order: 100, kirim: 20 },  // NEW MEMBER
+
+    // S40B (Slot Baru - 40 Hearts/Day)
+    { slot: "S40B", name: "NAI", order: 500, kirim: 40 },
+    { slot: "S40B", name: "WINDI", order: 500, kirim: 40 },
+    { slot: "S40B", name: "ASHA", order: 500, kirim: 40 },
+    { slot: "S40B", name: "mocchaa", order: 500, kirim: 40 }
 ];
